afd6cfc791d04d341abedc9eeab256952274549a
Shunya Kobayashi <shunya14dev@gmail.com>
2026-01-19 21:56:53 +0900

    プロジェクトファイルを追加。

---
+++ src/StudyManager/StudyManager.AppHost/AppHost.cs
@@ -0,0 +1,14 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+var apiService = builder.AddProject<Projects.StudyManager_ApiService>("apiservice")
+    .WithHttpHealthCheck("/health");
+
+builder.AddProject<Projects.StudyManager_Web>("webfrontend")
+    .WithExternalHttpEndpoints()
+    .WithHttpHealthCheck("/health")
+    .WithReference(apiService)
+    .WaitFor(apiService);
+
+builder.AddProject<Projects.TimeManager>("timemanager");
+
+builder.Build().Run();
