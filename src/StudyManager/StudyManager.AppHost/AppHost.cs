6ab2b7ec4136cd9e4c39e5b0fd0a39262f712cba
Shunya Kobayashi <shunya14dev@gmail.com>
2026-01-19 22:19:40 +0900

    BaseのBlazorアプリの削除

--- src/StudyManager/StudyManager.AppHost/AppHost.cs
+++ src/StudyManager/StudyManager.AppHost/AppHost.cs
@@ -3,12 +3,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 var apiService = builder.AddProject<Projects.StudyManager_ApiService>("apiservice")
     .WithHttpHealthCheck("/health");
 
-builder.AddProject<Projects.StudyManager_Web>("webfrontend")
-    .WithExternalHttpEndpoints()
-    .WithHttpHealthCheck("/health")
-    .WithReference(apiService)
-    .WaitFor(apiService);
-
 builder.AddProject<Projects.TimeManager>("timemanager").WithReference(apiService);
 
 builder.Build().Run();
