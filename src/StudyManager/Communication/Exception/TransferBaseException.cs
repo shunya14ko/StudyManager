f1708b6c53091896d69d9ce5b5ccd238b8225c94
Shunya Kobayashi <shunya14dev@gmail.com>
2026-02-02 22:17:56 +0900

    ちょっとした気分程度の修正

--- src/StudyManager/Communication/Exception/TransferBaseException.cs
+++ src/StudyManager/Communication/Exception/TransferBaseException.cs
@@ -1,7 +1,5 @@
 ﻿namespace TransferProtocol.Exception;
 
-// publicにして他プロジェクトから見えるようにする
-// abstractなので、このクラス自体はインスタンス化できない（設計通り）
 public abstract class TransferBaseException : System.Exception
 {
     public TransferBaseException(string message) : base(message) { }
