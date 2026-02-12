446a4d39ce474b8b83fd044c7608ad0713a7afe0
Shunya Kobayashi <shunya14dev@gmail.com>
2026-02-12 22:58:17 +0900

    DockerからLoki取得するページの作成

---
+++ src/TimeManager/LokiData/Model.cs
@@ -0,0 +1,14 @@
+﻿namespace TaskManager.LokiData;
+
+public class LogModel
+{
+    // Lokiが付与するタイムスタンプ用
+    public DateTime Timestamp { get; set; }
+
+    // 以下、PythonスクリプトのJSONの中身
+    public string Level { get; set; } = "";
+    public string Endpoint { get; set; } = "";
+    public int Status { get; set; }
+    public string Message { get; set; } = "";
+    public int DurationMs { get; set; }
+}
