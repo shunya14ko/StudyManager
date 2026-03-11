f124f5a45e719a3570beecded1e25f75abc59f6c
Shunya Kobayashi <shunya14dev@gmail.com>
2026-03-12 00:06:59 +0900

    Channelを触ってみる

---
+++ src/TimeManager/Services/SompleCreateBounded.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Channels;
+
+namespace TaskManager.Services;
+
+public class SampleCreateBounded
+{
+    public void Create()
+    {
+        // Create a bounded channel with a capacity of 1
+        var channel = Channel.CreateBounded<string>(1);
+
+        channel.Writer.WriteAsync("Hello, World!");
+        channel.Reader.ReadAllAsync();
+    }
+}
